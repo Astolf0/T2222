962e51ecdecf32ca639b21f9dacf10c275262a02
malka88 <malka_88@outlook.com>
2019-03-22 13:46:50 +0700

    delen

--- source/repos/ConsoleApp3/ConsoleApp3/Program.cs
+++ source/repos/ConsoleApp3/ConsoleApp3/Program.cs
@@ -8,6 +8,11 @@ namespace ConsoleApp3
 {
     class Program
     {
+        static double razdylitor(ref int a, ref int b)
+        {
+            double g = a / Convert.ToDouble(b);
+            return g;
+        }
         static void Main(string[] args)
         {
         }
