be0e5273826f655eaadd69d5d67335ffac7614df
malka88 <malka_88@outlook.com>
2019-03-22 13:48:13 +0700

    ymnoj

--- source/repos/ConsoleApp3/ConsoleApp3/Program.cs
+++ source/repos/ConsoleApp3/ConsoleApp3/Program.cs
@@ -8,6 +8,11 @@ namespace ConsoleApp3
 {
     class Program
     {
+        static int umno(ref int a, ref int b)
+        {
+            int g = a * b;
+            return g;
+        }
         static void Main(string[] args)
         {
         }
